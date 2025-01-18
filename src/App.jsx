9b836057fca3900d231cce35397d271a90daf6d8
akghosh111 <ghoshkiran2017@gmail.com>
2025-01-18 23:58:03 +0530

    added fade up/down

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {motion} from "framer-motion"
+import {easeOut, motion} from "framer-motion"
 
 const gridContainerVariants ={hidden: {opacity: 0},show: {opacity:1, transition: {staggerChildren:0.25,},},}
 
@@ -10,7 +10,11 @@ const App = () => {
     <div className="flex flex-col gap-10 overflow-x-hidden">
       <motion.section variants={gridContainerVariants} 
       initial="hidden" animate="show" className=' grid grid-cols-3 p-10 gap-10'>
-        <motion.div variants={gridSquareVariants} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
+        <motion.div variants={gridSquareVariants} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+          <motion.div className="w-20 h-20 bg-stone-100 rounded-lg"
+          initial={{opacity:0, y:100}} animate= {{opacity:1, y:0}} transition={{duration:1, ease:"easeOut", delay:"0.2"}}></motion.div>
+          <motion.div className="w-20 h-20 bg-stone-100 rounded-full" initial={{opacity:0, y:-100}} animate= {{opacity:1, y:0}} transition={{duration:1, ease:"easeOut", delay:"0.4"}}></motion.div>
+        </motion.div>
         <motion.div variants={gridSquareVariants} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
         <motion.div variants={gridSquareVariants} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
         <motion.div variants={gridSquareVariants} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
